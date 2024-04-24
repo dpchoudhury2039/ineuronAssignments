3fb0dbebd7a6687b91fb1e97e007dfcb8d0c0138
Deep Jyoti Choudhury <38779689+dpchoudhury2039@users.noreply.github.com>
2024-04-24 11:41:22 +0530

    Create Question3.py

---
+++ PythonAssignement1/Question3.py
@@ -0,0 +1,10 @@
+import math
+
+diameter = 12
+
+radius = diameter / 2
+
+# Calculate the volume using the formula
+volume = (4/3) * math.pi * (radius ** 3)
+
+print("The volume of the sphere with diameter 12 cm is:", volume)
