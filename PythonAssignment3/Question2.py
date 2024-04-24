c2f5a61e10db690853edd4e721d259af37ce7582
Deep Jyoti Choudhury <38779689+dpchoudhury2039@users.noreply.github.com>
2024-04-24 12:32:55 +0530

    Create Question2.py

---
+++ PythonAssignment3/Question2.py
@@ -0,0 +1,22 @@
+def myfilter(function, iterable):
+    filtered_results = []
+
+    for item in iterable:
+        # If the function returns True for this item, add it to the results
+        if function(item):
+            filtered_results.append(item)
+
+    return filtered_results
+
+
+def canvote(x):
+    if x < 18:
+        return False
+    else:
+        return True
+
+
+numbers = [1, 2, 33, 4, 25, 16, 7, 18, 19, 10]
+filtered_result = myfilter(canvote, numbers)
+
+print("Filtered result:", filtered_result)
